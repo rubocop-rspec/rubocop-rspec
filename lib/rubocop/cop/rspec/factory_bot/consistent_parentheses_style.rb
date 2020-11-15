c67d1c76cb7c17927595e49a1fd1dd1fa691964d
Nick Flueckiger <nick.flueckiger@renuo.ch>
2020-11-15 17:54:36 +0100

    Implement a rubocop for consistent usage of parens

---
+++ lib/rubocop/cop/rspec/factory_bot/consistent_parentheses_style.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+module RuboCop
+  module Cop
+    module RSpec
+      module FactoryBot
+        # Use a consistent style for parentheses in factory bot calls
+        #
+        # @example
+        #
+        #   # bad
+        #
+        #   create :user
+        #   build(:user)
+        #   create(:login)
+        #   create :login
+        #
+        #   # good
+        #   create :user
+        #   build :user
+        #   create :login
+        #   create :login
+        #
+        #   # good 
+        #   create(:user)
+        #   create(:user)
+        #   create(:login)
+        #   build(:login)
+        #
+        class ConsistentParenthesesStyle < Base
+          extend AutoCorrector
+          include ConfigurableEnforcedStyle
+
+          MSG_ENFORCE_PARENS = 'Prefer method call with parentheses'
+          MSG_OMIT_PARENS = 'Prefer method call without parentheses'
+
+
+          def_node_matcher :factory_call, <<-PATTERN
+            {
+              (send 
+                ${(const nil? {:FactoryGirl :FactoryBot}) nil?} :create (sym $_) 
+              $...)
+              (
+                send ${(const nil? {:FactoryGirl :FactoryBot}) nil?} :build (sym $_) 
+              $...)
+            }
+          PATTERN
+
+          def on_send(node)
+            parent = node.parent
+            # prevent from nested matching
+            if parent.respond_to?('method_name')
+              method_name = parent.method_name
+              if method_name == :build || method_name == :create
+                return
+              end
+            end
+            factory_call(node) do
+              if node.parenthesized?
+                return if style == :enforce_parentheses
+                add_offense(node.loc.selector, message: MSG_OMIT_PARENS) do |corrector|
+                  autocorrect_remove_parens(corrector, node)
+                end
+              else
+                return if style == :omit_parentheses
+                add_offense(node.loc.selector, message: MSG_ENFORCE_PARENS) do |corrector|
+                  autocorrect_enforce_parens(corrector, node)
+                end
+              end
+            end
+          end
+
+          private
+
+          def autocorrect_remove_parens(corrector, node)
+            corrector.replace(node.location.begin, ' ')
+            corrector.remove(node.location.end)
+          end
+
+          def autocorrect_enforce_parens(corrector, node)
+            corrector.replace(args_begin(node), '(')
+            corrector.replace(args_end(node), ')')
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/rspec/factory_bot/consistent_parantheses_style_spec.rb
@@ -0,0 +1,182 @@
+# frozen_string_literal: true
+RSpec.describe RuboCop::Cop::RSpec::FactoryBot::ConsistentParenthesesStyle , :config do
+
+  let(:cop_config) do
+    { 'EnforcedStyle' => enforced_style }
+  end
+
+  context 'when EnforcedStyle is :enforce_parentheses' do
+    let(:enforced_style) { :enforce_parentheses }
+
+    context 'with create' do
+      it 'it flags the call to use parentheses' do
+        expect_offense(<<~RUBY)
+          create :user 
+          ^^^^^^ Prefer method call with parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          create(:user) 
+        RUBY
+      end
+    end
+
+    context 'with build' do
+      it 'it flags the call to use parentheses' do
+        expect_offense(<<~RUBY)
+          build :user 
+          ^^^^^ Prefer method call with parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          build(:user) 
+        RUBY
+      end
+    end
+
+    context 'with nested calling' do
+      it 'it flags the call to use parentheses' do
+        expect_offense(<<~RUBY)
+          build :user, build(:yester)
+          ^^^^^ Prefer method call with parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          build(:user, build(:yester))
+        RUBY
+      end
+
+      it 'works in a bigger context' do
+        expect_offense(<<~RUBY)
+          class Context
+            let(:build) { create :user, build(:user) } 
+                          ^^^^^^ Prefer method call with parentheses
+
+            it 'test in test' do
+              user = create :user, first: name, peter: miller
+                     ^^^^^^ Prefer method call with parentheses
+            end
+
+            let(:build) { create :user, build(:user, create(:user, create(:first_name))) } 
+                          ^^^^^^ Prefer method call with parentheses
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          class Context
+            let(:build) { create(:user, build(:user)) } 
+
+            it 'test in test' do
+              user = create(:user, first: name, peter: miller)
+            end
+
+            let(:build) { create(:user, build(:user, create(:user, create(:first_name)))) } 
+          end
+        RUBY
+      end
+    end
+
+    context 'with already valid usage of parentheses' do
+      it 'does not flag as invalid - create' do
+        expect_no_offenses(<<~RUBY)
+          create(:user) 
+        RUBY
+      end
+
+      it 'does not flag as invalid - build' do
+        expect_no_offenses(<<~RUBY)
+          build(:user) 
+        RUBY
+      end
+    end
+  end
+
+
+  context 'when EnforcedStyle is :omit_parentheses' do
+    let(:enforced_style) { :omit_parentheses}
+
+    context 'with create' do
+
+      it 'flags the call to not use parentheses' do
+        expect_offense(<<~RUBY)
+          create(:user)
+          ^^^^^^ Prefer method call without parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          create :user
+        RUBY
+      end
+
+    end
+
+    context 'with build' do
+      it 'flags the call to not use parentheses' do
+        expect_offense(<<~RUBY)
+          build(:user)
+          ^^^^^ Prefer method call without parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          build :user
+        RUBY
+      end
+    end
+
+    context 'with nested calling' do
+      it 'it flags the call to use parentheses' do
+        expect_offense(<<~RUBY)
+          build(:user, build(:yester))
+          ^^^^^ Prefer method call without parentheses
+        RUBY
+
+        expect_correction(<<~RUBY)
+          build :user, build(:yester)
+        RUBY
+      end
+    end
+
+    context 'with already valid usage of parentheses' do
+      it 'does not flag as invalid - create' do
+        expect_no_offenses(<<~RUBY)
+          create :user
+        RUBY
+      end
+
+      it 'does not flag as invalid - build' do
+        expect_no_offenses(<<~RUBY)
+          build :user
+        RUBY
+      end
+    end
+
+    it 'works in a bigger context' do
+      expect_offense(<<~RUBY)
+        class Context
+          let(:build) { create(:user, build(:user)) } 
+                        ^^^^^^ Prefer method call without parentheses
+
+          it 'test in test' do
+            user = create(:user, first: name, peter: miller)
+                   ^^^^^^ Prefer method call without parentheses
+          end
+
+          let(:build) { create(:user, build(:user, create(:user, create(:first_name)))) } 
+                        ^^^^^^ Prefer method call without parentheses
+        end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        class Context
+          let(:build) { create :user, build(:user) } 
+
+          it 'test in test' do
+            user = create :user, first: name, peter: miller
+          end
+
+          let(:build) { create :user, build(:user, create(:user, create(:first_name))) } 
+        end
+      RUBY
+    end
+  end
+end
